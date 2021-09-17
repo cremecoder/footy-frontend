bd0ad985e2dd61ced606c934122087ddd65a5995
Sean <sean.cremecoder@gmail.com>
2021-09-17 15:51:44 +0100

    added functions.js and split some functionality in script.js

---
+++ src/js/functions.js
@@ -0,0 +1,8 @@
+// export function connectToApi() {
+//   fetch("http://localhost:9000/api/worldcup")
+//     .then(res => res.json())
+//     .then(data => {
+//       return data
+//     })
+//     .catch(err => console.log("Error: " + err))
+// }
